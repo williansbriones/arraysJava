8caee7a3d4f46f2a313a5724a8d722b26f8ca4b7
Cetecom <Cetecom@LC160721>
2022-09-23 22:06:18 -0300

    a

---
+++ src/main/java/cl/duoc/democolecciones/Main.java
@@ -0,0 +1,19 @@
+
+package cl.duoc.democolecciones;
+
+import java.util.ArrayList;
+
+public class Main {
+    
+    public static void main(String[] args) {
+       
+        ArrayList<String> lista = new ArrayList<>();
+        
+        lista.add("pepe");
+        
+        System.out.println("el numero " + lista.size());
+        
+        
+    }
+    
+}
